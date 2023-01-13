aa85415b06a3b7cf95fd9a412843087a03031542
Lucas Gomes <lggomes007@gmail.com>
2023-01-12 23:38:00 -0300

    adicionando header

---
+++ incash/src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h2>In Cash</h2>
+            <span>blablablabalbal</span>
+        </div>
+    );
+}
+
+export default Header;
